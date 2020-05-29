13afc7cfd9cf0299703bbb91d75f837bd6323d87
Priyanka Sachan <56231576+Priyanka-Sachan@users.noreply.github.com>
2020-05-29 19:14:47 +0530

    Initial Commit

---
+++ alien_invasion.py
@@ -0,0 +1,14 @@
+import sys
+import pygame
+def run_game():
+    pygame.init()
+    screen=pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("ALIEN INVASION")
+    bg_color=(230,230,230)
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                sys.exit()
+        screen.fill(bg_color)
+        pygame.display.flip()
+run_game()
